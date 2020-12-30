3bd0fb1ab93e57812cb210612882de2bd2c4c301
David Hodgson <hodgsondh@gmail.com>
2020-12-30 13:20:26 -0500

    changed app text'

--- src/App.js
+++ src/App.js
@@ -8,7 +8,9 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
-          I was changed
+        </p>
+        <p>
+          Change on feature branch
         </p>
         <a
           className="App-link"
